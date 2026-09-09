b341a50c3692925b1328c6facc77fdce7f6c2d73
Prantik Das <dasprantik76@gmail.com>
2026-09-09 23:52:44 +0530

    Use academy production API domain

--- site-config.js
+++ site-config.js
@@ -4,7 +4,7 @@
  */
 window.PUBLIC_SITE_CONFIG = Object.freeze({
   academySlug: 'prantik',
-  apiBaseUrl: 'https://academy-admin-portal.vercel.app',
-  adminPortalUrl: 'https://academy-admin-portal.vercel.app',
+  apiBaseUrl: 'https://academy.pixelsetu.com',
+  adminPortalUrl: 'https://academy.pixelsetu.com',
   certificateTemplateUrl: 'assets/diganta-certificate-template.jpg?v=2'
 });
